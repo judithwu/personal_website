11f72f177bce8f946d62b57e6c61c68f6fad8c68
judith <judithwu77@gmail.com>
2025-11-21 18:46:04 -0500

    setup tailwindcss and theme

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen ">
+
+    {/* theme toggle*/}
+
+    {/* bg effects */}
+
+    {/* navbar */}
+
+    {/* main content */}
+
+    {/* footer */}
+    </div>
+  );
+};
