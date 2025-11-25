46aaa3f8e7cdee31c570e0591001870781bdbf89
judith <judithwu77@gmail.com>
2025-11-25 14:25:25 -0500

    setup background effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
 export const Home = () => {
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
     {/* theme toggle*/}
-
+    <ThemeToggle />
     {/* bg effects */}
+    <StarBackground />
 
     {/* navbar */}
 
