f21a0d4b2d6c557bb5115bf852fa9c5f051fe16d
judith <judithwu77@gmail.com>
2025-12-01 05:05:53 -0500

    deploying to gh pages

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,11 +11,11 @@ export const HeroSection = () => {
             <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
               <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
               <span className="text-primary opacity-0 animate-fade-in-delay-1"> Judith</span>
-              <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Wu</span>
+              
             </h1>
-            <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+            {/*<p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
               Bio here
-            </p>
+            </p>*/}
 
             <div className="pt-4 opacity-0 animate-fade-in-delay-4">
               <a href="#projects" className="space-button">
