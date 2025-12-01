8049d9f94edef8b14b31dc1e6a081dddfd85519c
judith <judithwu77@gmail.com>
2025-12-01 04:45:28 -0500

    contact section finished

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,5 +1,104 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
+const projects = [
+  {
+    id: 1,
+    title: "Semantic Segmentation of Road Scenes",
+    description: "lorem ipsum dolor",
+    image: "/projects/proj3.png",
+    tags: ["PyTorch", "blah", "bleh"],
+    demoUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    id: 2,
+    title: "Semantic Segmentation of Road Scenes",
+    description: "lorem ipsum dolor",
+    image: "/projects/proj2.png",
+    tags: ["PyTorch", "blah", "bleh"],
+    demoUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    id: 3,
+    title: "Semantic Segmentation of Road Scenes",
+    description: "lorem ipsum dolor",
+    image: "/projects/proj1.png",
+    tags: ["PyTorch", "blah", "bleh"],
+    demoUrl: "#",
+    githubUrl: "#"
+  },
+];
 
 export const ProjectsSection = () => {
-  return <div></div>;
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> 
+          Featured <span className="text-primary"> Projects </span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some projects I have done.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div 
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                <div className="h-48 overflow-hidden">
+                  <img 
+                    src={project.image} 
+                    alt={project.title} 
+                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"></img>
+
+                </div>
+                <div className="p-6">
+                  <div className="flex flex-wrap gap-2 mb-4">
+                    {project.tags.map((tag) => (
+                      <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
+                        {tag}
+                      </span>
+                    ))}
+
+                  </div>
+                
+        
+                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3> 
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>    
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a 
+                      href={project.demoUrl} 
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                        <ExternalLink size={20}/>
+                    </a>
+                    <a 
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                        <Github size={20}/>
+                    </a>
+                  </div>
+                </div>
+              </div> 
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a 
+            className="space-button w-fit flex items-center mx-auto gap-2" 
+            target="_blank"
+            href="https://github.com/judithwu">
+            My GitHub <ArrowRight size={16}/>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
